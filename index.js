bbeaeee1252057be4b5c564610c83381c927d730
Progammer <programmer@nbadigitalworlds.com>
2023-09-12 13:03:26 +0700

    Update index.js

--- index.js
+++ index.js
@@ -15,59 +15,59 @@ app.use(express.json());
 app.use(cors());
 
 //Member
-app.use('/v3/nba-platform', require("./routes/index"));
-app.use('/v3/nba-platform/member', require("./routes/member"));
+app.use('/v2/nba-platform', require("./routes/index"));
+app.use('/v2/nba-platform/member', require("./routes/member"));
 
 //Wallet เติมเงินเข้ากระเป๋า
-app.use('/v3/nba-platform/wallet', require('./routes/wallet'));
+app.use('/v2/nba-platform/wallet', require('./routes/wallet'));
 
 //withdraw ถอนเงิน
-app.use('/v3/nba-platform/withdraw', require('./routes/withdraw'));
+app.use('/v2/nba-platform/withdraw', require('./routes/withdraw'));
 
 //แจ้งเตือน
-app.use('/v3/nba-platform/notify', require('./routes/notify'));
+app.use('/v2/nba-platform/notify', require('./routes/notify'));
 
 //ประวัติค่าคอมมิชชั่น Money History
-app.use('/v3/nba-platform/money/history', require('./routes/money.history'));
+app.use('/v2/nba-platform/money/history', require('./routes/money.history'));
 
 //ปวะวัติ wallet
-app.use('/v3/nba-platform/wallet/history', require('./routes/wallet.history'));
+app.use('/v2/nba-platform/wallet/history', require('./routes/wallet.history'));
 
 //Admin
-app.use('/v3/nba-platform/admin', require('./routes/admin/index'));
-app.use('/v3/nba-platform/admin/user', require('./routes/admin/user'));
-app.use('/v3/nba-platform/admin/member', require('./routes/admin/member'));
-app.use('/v3/nba-platform/admin/wallet', require('./routes/admin/wallet'));
-app.use('/v3/nba-platform/admin/withdraw',require('./routes/admin/withdraw'));
+app.use('/v2/nba-platform/admin', require('./routes/admin/index'));
+app.use('/v2/nba-platform/admin/user', require('./routes/admin/user'));
+app.use('/v2/nba-platform/admin/member', require('./routes/admin/member'));
+app.use('/v2/nba-platform/admin/wallet', require('./routes/admin/wallet'));
+app.use('/v2/nba-platform/admin/withdraw',require('./routes/admin/withdraw'));
 //Public
-app.use('/v3/nba-platform/public/member' ,require('./routes/public/member'));
-app.use('/v3/nba-platform/public/report', require('./routes/public/report'));
+app.use('/v2/nba-platform/public/member' ,require('./routes/public/member'));
+app.use('/v2/nba-platform/public/report', require('./routes/public/report'));
 
 //SMS
-app.use('/v3/nba-platform/sms/otp', require('./routes/sms/otp'));
+app.use('/v2/nba-platform/sms/otp', require('./routes/sms/otp'));
 
 //Counter service
-app.use('/v3/nba-platform/counter_service/mobile_topup',require('./routes/counterservice/mobile.topup'));
-app.use('/v3/nba-platform/counter_service/card_topup',require('./routes/counterservice/card.topup'));
-app.use('/v3/nba-platform/counter_service/mobile_bill',require('./routes/counterservice/mobile.bill'));
-app.use('/v3/nba-platform/counter_service/barcode_service',require('./routes/counterservice/barcode'));
-app.use('/v3/nba-platform/counter_service/nba_service',require('./routes/counterservice/nba'));
-app.use('/v3/nba-platform/counter_service/wallet',require('./routes/counterservice/wallet'));
+app.use('/v2/nba-platform/counter_service/mobile_topup',require('./routes/counterservice/mobile.topup'));
+app.use('/v2/nba-platform/counter_service/card_topup',require('./routes/counterservice/card.topup'));
+app.use('/v2/nba-platform/counter_service/mobile_bill',require('./routes/counterservice/mobile.bill'));
+app.use('/v2/nba-platform/counter_service/barcode_service',require('./routes/counterservice/barcode'));
+app.use('/v2/nba-platform/counter_service/nba_service',require('./routes/counterservice/nba'));
+app.use('/v2/nba-platform/counter_service/wallet',require('./routes/counterservice/wallet'));
 
 //NBA service
-app.use('/v3/nba-platform/nbaservice', require('./routes/NBAservice/index'))
+app.use('/v2/nba-platform/nbaservice', require('./routes/NBAservice/index'))
 
 // Order
-app.use('/v3/nba-platform/order', require('./routes/order'))
+app.use('/v2/nba-platform/order', require('./routes/order'))
 
 // commission
-app.use('/v3/nba-platform/commission', require('./routes/commission'))
+app.use('/v2/nba-platform/commission', require('./routes/commission'))
 
 // exchange
-app.use('/v3/nba-platform/exchangepoint', require('./routes/exchange/index'))
+app.use('/v2/nba-platform/exchangepoint', require('./routes/exchange/index'))
 
 //artwork
-app.use('/v3/nba-platform/artwork',require('./routes/artwork/artwork'));
+app.use('/v2/nba-platform/artwork',require('./routes/artwork/artwork'));
 
 //
 const port = process.env.PORT || 9100;
